41806c470407269dd0548f4a8d6cb5f556d6a1d2
Dmdiamond79 <135099351+Dmdiamond79@users.noreply.github.com>
2024-02-09 13:41:31 +0700

    :revisistrukturfolderdankonflik (#12)

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const ProductRouter = require('./productRoute')
-
-router.use('/api/products', ProductRouter);
-=======
-const UserRouter = require('./userRoute');
-const AdminRouter = require('./adminRoute');
-const AuthRouter = require('./authRoute');
+const UserRouter = require('./user');
+const AdminRouter = require('./admin');
+const AuthRouter = require('./auth');
+const ProductRouter = require('./product');
+const warehouseRouter = require("./warehouse");
 const auth = require('../middleware/auth');
+
 
 router.use('/api/auth', AuthRouter);
 router.use(auth);
 router.use('/api/users', UserRouter);  
 router.use('/api/admin', AdminRouter);
->>>>>>> development
+router.use('/api/products', ProductRouter);
+router.use('/warehouses', warehouseRouter);
 
 module.exports = router;
