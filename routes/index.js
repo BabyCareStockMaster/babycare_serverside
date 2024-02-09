23aec784629be6803536008a12beaa2b64322c41
danijuhaeni22 <danijuhaeni22@gmail.com>
2024-02-09 14:13:29 +0700

    add feature category

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserRouter = require('./userRoute');
-const AdminRouter = require('./adminRoute');
-const AuthRouter = require('./authRoute');
-const auth = require('../middleware/auth');
+const UserRouter = require("./user");
+const AdminRouter = require("./admin");
+const CategoryRouter = require("./category");
+const AuthRouter = require("./auth");
+const auth = require("../middleware/auth");
 
 router.use('/api/auth', AuthRouter);
 router.use(auth);
 router.use('/api/users', UserRouter);  
 router.use('/api/admin', AdminRouter);
+router.use('/api/category', CategoryRouter);
 
 module.exports = router;
