3866cc2d28bdf98602d49c96c76c512b7dd36c51
Muhammad Furqan <muhammadfurqan7427@gmail.com>
2024-02-21 21:38:16 +0700

    added schedule

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ const errorHandler = require('./middleware/errorHandler');
 const port = 4000
 const app = express()
 const path = require('path');
+const cron = require('node-cron');
+const WarehouseStockController = require('./controller/warehouseStockController');
 
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router)
+// cron.schedule('*/5 * * * * *', () => {
+//     WarehouseStockController.checkEmptyStock()
+//     WarehouseStockController.checkSurplusStock()
+// })
+cron.schedule('0 12 * * *', () => {
+    console.log('Running the task at 12 PM every day!');
+    // Place your task logic here
+    WarehouseStockController.checkEmptyStock()
+    WarehouseStockController.checkSurplusStock()
+  }, {
+    timezone: 'Asia/Jakarta' // Change 'Your/Timezone' to your desired timezone
+  });
 app.use(errorHandler)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.listen(port, () => {
